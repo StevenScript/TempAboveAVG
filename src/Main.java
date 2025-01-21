713d73bb0683fb0c386ca59d994043d1c0cad2c8
StevenScript <sdn709466@gmail.com>
2025-01-21 12:10:40 -0330

    +Prompt For Num. Of Temps
    + Prompt for number of temperatures to be entered.
    
    - Replaced test print message with relevant code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 package src;
 
+import java.util.Scanner;
+
 // Main class to execute project's code
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Displaying the program's start message to the user
-        System.out.println("Starting Temperature Above Average program...");
+        System.out.print("How many temperatures do you want to enter? ");
+        int n = scanner.nextInt();  // read integer input
+
+        System.out.println("You will enter " + n + " temperature values.");
     }
 }
