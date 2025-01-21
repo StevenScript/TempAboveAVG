d0ee894700820e72cdf9a0c157ba289adbfca438
StevenScript <sdn709466@gmail.com>
2025-01-21 03:35:27 -0330

    +Placeholder Main Class
    Main class with placeholder main method.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package src;
+
+// Main class to execute project's code
+public class Main {
+    public static void main(String[] args) {
+        // Displaying the program's start message to the user
+        System.out.println("Starting Temperature Above Average program...");
+    }
+}
