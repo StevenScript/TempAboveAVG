47d8a24d30cba86255dbf525366d8d93aff21b3c
StevenScript <sdn709466@gmail.com>
2025-01-21 12:44:56 -0330

    +Loop to Store Temp Data In Array
    +Added a loop for storing temperature data in an array
    
    +improved comments

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,26 @@ import java.util.Scanner;
 // Main class to execute project's code
 public class Main {
     public static void main(String[] args) {
+
+        // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
-        // Displaying the program's start message to the user
+        // Prompt user to input the number of temperature values being entered
         System.out.print("How many temperatures do you want to enter? ");
-        int n = scanner.nextInt();  // read integer input
+        int n = scanner.nextInt();  // read integer input for the number of temperatures
 
-        System.out.println("You will enter " + n + " temperature values.");
+        // Array to store the temperature received as
+        double[] temperatures = new double[n];
+
+        // Acknowledge the previous input and inform user to enter each individual temperature
+        System.out.println("Please enter " + n + " temperature values:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Temperature " + (i + 1) + ": ");
+            temperatures[i] = scanner.nextDouble();  // store each input
+        }
+
+        // Just a confirmation
+        System.out.println("Temperatures successfully recorded!");
+
     }
 }
